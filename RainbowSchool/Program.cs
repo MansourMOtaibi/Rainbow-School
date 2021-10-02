da2abee98250f4452d6c843574b26dd8645b579f
Mansour Alotaibi <MansourMOtaibi@gmail.com>
2021-10-02 03:56:41 +0300

    Update Teacher By ID

--- RainbowSchool/Program.cs
+++ RainbowSchool/Program.cs
@@ -23,6 +23,11 @@ namespace RainbowSchool
             public string Name { get; set; }
             public int Class { get; set; }
             public string Section { get; set; }
+
+            public string ToOneLine()
+            {
+                return $"{ID},{Name},{Class},{Section}";
+            }
         }
 
         class DataWarehouse
@@ -76,7 +81,7 @@ namespace RainbowSchool
             {
                 using (StreamWriter sw = File.AppendText(TextFilePath))
                 {
-                    sw.WriteLine($"{_teacher.ID},{_teacher.Name},{_teacher.Class},{_teacher.Section}");
+                    sw.WriteLine(_teacher.ToOneLine());
                     this.Teachers.Add(_teacher);
                 }
             }
@@ -98,6 +103,35 @@ namespace RainbowSchool
                     i++;
                 }
                 Console.WriteLine("*** There Are No Teacher With This ID !!");
+            }
+
+            // Updating Teacher By ID
+            public void UpdateTeacher(int _id , Teacher _teacher)
+            {
+                foreach (var teacher in this.Teachers)
+                {
+                    if(teacher.ID == _id)
+                    {
+                        teacher.ID = _teacher.ID;
+                        teacher.Name = _teacher.Name;
+                        teacher.Class = _teacher.Class;
+                        teacher.Section = _teacher.Section;
+                        File.WriteAllLines(TextFilePath, TeachersToLinesArray());
+                        GetTeachersDataFromTXT();
+                    }
+                }
+            }
+
+            // convert List of Teachers to lines array
+            private string[] TeachersToLinesArray()
+            {
+                List<string> _teachers = new List<string>();
+                foreach (var teacher in this.Teachers)
+                {
+                    _teachers.Add(teacher.ToOneLine());
+                }
+
+                return _teachers.ToArray();
             }
 
             // Append all teachers' Data 
@@ -127,16 +161,18 @@ namespace RainbowSchool
             var DB = new DataWarehouse();
             DB.DisplayTeachers();
             Console.ReadKey();
-            DB.AddTeacher(new Teacher(6, "Faisal Mohammed", 101, "Math"));
-            DB.AddTeacher(new Teacher(8, "Saad Mohammed", 101, "Math"));
-            DB.AddTeacher(new Teacher(6, "Ahmed Mohammed", 304, "Math"));
-            DB.AddTeacher(new Teacher(6, "Somone Mohammed", 505, "Math"));
-            DB.DisplayTeachers();
-            Console.ReadKey();
+            //DB.AddTeacher(new Teacher(6, "Faisal Mohammed", 101, "Math"));
+            //DB.AddTeacher(new Teacher(8, "Saad Mohammed", 101, "Math"));
+            //DB.AddTeacher(new Teacher(6, "Ahmed Mohammed", 304, "Math"));
+            //DB.AddTeacher(new Teacher(6, "Somone Mohammed", 505, "Math"));
+            //DB.DisplayTeachers();
+            //Console.ReadKey();
             DB.DeleteTeacher(8);
             DB.DisplayTeachers();
             Console.ReadKey();
-
+            DB.UpdateTeacher(7,new Teacher(11, "Mansour", 511, "CS"));
+            DB.DisplayTeachers();
+            Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
